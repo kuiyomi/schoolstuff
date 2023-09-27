b95f84142c55ca8eb2a5c6240a58e9acb552319e
kuiyomi <141984045+kuiyomi@users.noreply.github.com>
2023-09-27 01:10:14 -0300

    Create exercicio1aL2.java

---
+++ assignments/exerciselist2/exercicio1aL2.java
@@ -0,0 +1,20 @@
+public class exercicio1aL2 {
+    public static void main(String args[]) {
+    boolean expr1, expr2;
+
+    System.out.println("\fTabela Verdade do Operador ||");
+    System.out.println("Expr1\tExpr2\tExpr1 || Expr2");
+    
+    expr1 = false; expr2 = false;
+    System.out.println(expr1+"\t"+expr2+"\t"+(expr1 || expr2));
+    
+    expr1 = false; expr2 = true;
+    System.out.println(expr1+"\t"+expr2+"\t"+(expr1 || expr2));
+    
+    expr1 = true; expr2 = false;
+    System.out.println(expr1+"\t"+expr2+"\t"+(expr1 || expr2));
+    
+    expr1 = true; expr2 = true;
+    System.out.println(expr1+"\t"+expr2+"\t"+(expr1 || expr2));
+    }
+}
