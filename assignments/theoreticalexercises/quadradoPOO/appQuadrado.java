c3b1b25255f0bdbaf460d570f0cac95b3f691c8c
kuiyomi <141984045+kuiyomi@users.noreply.github.com>
2023-10-18 18:41:22 -0300

    Create appQuadrado.java

---
+++ assignments/theoreticalexercises/quadradoPOO/appQuadrado.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class appQuadrado {
+    public static void main (String args[]) {
+        Scanner in = new Scanner(System.in);
+        double lado;
+        String cor;
+        
+        Quadrado quad1 = new Quadrado();
+        System.out.println("O lado do quadrado é " + quad1.getLado());
+
+        System.out.println("Informe o valor do novo lado: ");
+        lado = in.nextDouble();
+        quad1.setLado(lado);
+        System.out.println("Agora, o lado do quadrado é " + quad1.getLado());
+
+        System.out.println("Informe a nova cor para o quadrado: ");
+        in.nextLine();
+        cor = in.nextLine();
+        quad1.setCor(cor);
+        System.out.println("A cor do quadrado é " + quad1.getCor());
+
+        System.out.println("A área do quadrado é " + quad1.calculaArea());
+        System.out.println("O perímetro do quadrado é " + quad1.calculaPerimetro());
+    }
+}
