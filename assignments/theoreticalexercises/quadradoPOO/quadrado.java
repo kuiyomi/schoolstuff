76c91304c9259987772d41f0c0de3753e8cf346b
kuiyomi <141984045+kuiyomi@users.noreply.github.com>
2023-10-18 18:41:50 -0300

    Create quadrado.java

---
+++ assignments/theoreticalexercises/quadradoPOO/quadrado.java
@@ -0,0 +1,33 @@
+import java.security.KeyStore.LoadStoreParameter;
+
+public class quadrado {
+    
+    private double lado;
+    private String cor;
+
+    public Quadrado () {
+        lado = 0;
+        cor = " ";
+    }
+
+    public double getLado () {
+        return lado;
+    }
+
+    public String getCor () {
+        return cor;
+    }
+
+    public void setLado (double novoLado) {
+        lado = novoLado;
+    }
+    public void setCor (String novaCor) {
+        cor = novaCor;
+    }
+    public double calculaArea() {
+        return lado * lado;
+    }
+    public double calculaPerimetro() {
+        return lado * 4;
+    }
+}
