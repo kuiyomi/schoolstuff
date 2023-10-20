c6e7ed661be0f552149d211c31a3ea2d1aefde6c
kuiyomi <141984045+kuiyomi@users.noreply.github.com>
2023-10-20 18:43:30 -0300

    Create appRetangulo.java

---
+++ assignments/theoreticalexercises/retanguloPOO/appRetangulo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AppRetangulo
+{
+    public static void main(String args[]){
+        Scanner in = new Scanner (System.in);
+        double altura,base;
+        
+        Retangulo retangulo1 = new Retangulo();
+        
+        System.out.println("\fDigite a altura: ");
+        altura = in.nextDouble();
+        retangulo1.setAltura(altura);
+        System.out.println("Digite a base: ");
+        base = in.nextDouble();
+        retangulo1.setBase(base);
+    
+        System.out.println(retangulo1.toString());
+          
+    }
+}
