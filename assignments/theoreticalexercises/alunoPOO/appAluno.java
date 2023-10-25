6ce249c055426fd40f806aa09a6e6be4800dfefd
kuiyomi <141984045+kuiyomi@users.noreply.github.com>
2023-10-25 17:55:17 -0300

    Update appAluno.java

--- assignments/theoreticalexercises/alunoPOO/appAluno.java
+++ assignments/theoreticalexercises/alunoPOO/appAluno.java
@@ -5,21 +5,27 @@ public class AppAluno
         double nota;
         int frequencia;
         Aluno a1 = new Aluno();
+        
         System.out.println("Digite a p1 do aluno: ");
         nota = in.nextDouble();
         a1.setP1(nota);
+        
         System.out.println("Digite a p2 do aluno: ");
         nota = in.nextDouble();
         a1.setP2(nota);
+        
         System.out.println("Digite a p3 do aluno: ");
         nota = in.nextDouble();
         a1.setP3(nota);
+        
         System.out.println("Digite a me do aluno: ");
         nota = in.nextDouble();
         a1.setMe(nota);
+        
         System.out.println("Digite a frquencia do aluno: ");
         frequencia = in.nextInt();
         a1.setFrequencia(frequencia);
+        
         if (a1.mostraResultadoG1().equalsIgnoreCase("Em G2")){
             System.out.println("Digite a g2 do aluno: ");
             nota = in.nextDouble();
