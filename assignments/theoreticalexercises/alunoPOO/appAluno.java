94ce735258630c204feacdd38e1f5c598f4ccf90
kuiyomi <141984045+kuiyomi@users.noreply.github.com>
2023-10-25 17:54:57 -0300

    Update appAluno.java

--- assignments/theoreticalexercises/alunoPOO/appAluno.java
+++ assignments/theoreticalexercises/alunoPOO/appAluno.java
@@ -1,43 +1,31 @@
-import java.util.Scanner;
-
-public class appAluno {
-    public static void main (String args[]) {
-        Scanner in = new Scanner (System.in);
-        double p1, p2, p3, me, frq, g2;
-        int mat;
-        String nome;
-
-        Aluno aluno1 = new Aluno();
-
-        System.out.println("Informe o nome do aluno: ");
-        nome = in.nextLine();
-        aluno1.setNome(nome);
-
-        System.out.println("Informe a matricula do aluno: ");
-        mat = in.nextInt();
-        aluno1.setMat(mat);
-
-        System.out.println("Informe a frequência do aluno: ");
-        frq = in.nextDouble();
-        aluno1.setFrq(frq);
-
-        System.out.println("Informe a nota da primeira prova do aluno: ");
-        p1 = in.nextDouble();
-        aluno1.setP1(p1);
-
-        System.out.println("Informe a nota da segunda prova do aluno: ");
-        p2 = in.nextDouble();
-        aluno1.setP2(p2);
-
-        System.out.println("Informe a nota da terceira prova do aluno: ");
-        p3 = in.nextDouble();
-        aluno1.setP3(p3);
-
-        System.out.println("Informe a nota da média de exercícios do aluno: ");
-        me = in.nextDouble();
-        aluno1.setME(me);
-
-        System.out.println(aluno1.toString());
-    }
+public class AppAluno
+{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        double nota;
+        int frequencia;
+        Aluno a1 = new Aluno();
+        System.out.println("Digite a p1 do aluno: ");
+        nota = in.nextDouble();
+        a1.setP1(nota);
+        System.out.println("Digite a p2 do aluno: ");
+        nota = in.nextDouble();
+        a1.setP2(nota);
+        System.out.println("Digite a p3 do aluno: ");
+        nota = in.nextDouble();
+        a1.setP3(nota);
+        System.out.println("Digite a me do aluno: ");
+        nota = in.nextDouble();
+        a1.setMe(nota);
+        System.out.println("Digite a frquencia do aluno: ");
+        frequencia = in.nextInt();
+        a1.setFrequencia(frequencia);
+        if (a1.mostraResultadoG1().equalsIgnoreCase("Em G2")){
+            System.out.println("Digite a g2 do aluno: ");
+            nota = in.nextDouble();
+            a1.setG2(nota);
+            System.out.println(a1.mostraResultadoG2());
+        } else 
+            System.out.println(a1.mostraResultadoG1());
+        }
 }
-...
