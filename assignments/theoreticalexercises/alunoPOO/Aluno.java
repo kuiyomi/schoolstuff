a797d06cc8058aa677945dc2c767b770a4453bc8
kuiyomi <141984045+kuiyomi@users.noreply.github.com>
2023-10-25 17:51:11 -0300

    Update Aluno.java

--- assignments/theoreticalexercises/alunoPOO/Aluno.java
+++ assignments/theoreticalexercises/alunoPOO/Aluno.java
@@ -1,97 +1,75 @@
 public class Aluno {
-    private double p1, p2, p3, me, frq, g2;
-    private int mat;
-    private String nome;
-
-    public Aluno() {
-        p1 = 0;
-        p2 = 0;
-        p3 = 0;
-        me = 0;
-        g2 = 0;
-        frq = 0;
-        mat = 0;
-        nome = "";
-    }
-
-    public double getP1() {
-        return p1;
+    private double p1,p2,p3,me,g2;
+    private int frequencia=0;
+    
+    
+    public Aluno(){
+        p1=0;
+        p2=0;
+        p3=0;
+        me=0;
+        frequencia=0;
+        g2=0;
     }
     
-    public double getP2() {
-        return p2;
+    public void setP1(double novaP1){
+        p1 = novaP1;
     }
     
-    public double getP3() {
-        return p3;
+    public double getP1(){return p1;
     }
-
-    public double getME() {
-        return me;
+    
+    public void setP2(double novaP2){
+        p2 = novaP2;
     }
-
-    public double getG2() {
-        return g2;
+    
+    public double getP2(){return p2;
     }
-
-    public double getFrq() {
-        return frq;
+    
+    public void setP3(double novaP3){
+        p3 = novaP3;
     }
-
-    public int getMat() {
-        return mat;
+    
+    public double getP3(){return p3;
     }
-
-    public String getNome() {
-        return nome;
+    
+    public void setMe(double novaMe){
+        me = novaMe;
     }
-
-    public void setP1(double p1) {
-        this.p1 = p1;
+    
+    public double getMe(){return me;
     }
-
-    public void setP2(double p2) {
-        this.p2 = p2;
+    
+    public void setFrequencia(int novaFrequencia){
+        frequencia = novaFrequencia;
     }
-
-    public void setP3(double p3) {
-        this.p3 = p3;
+    
+    public void setG2(double novaG2){
+        g2 = novaG2;;
     }
-
-    public void setME(double me) {
-        this.me = me;
+    
+    public double calculaG1(){
+        return (p1+p2+p3+me)/4;
     }
-
-    public void setG2(double g2) {
-        this.g2 = g2;
+    
+    public double calculaG2(){
+        return (calculaG1()+g2)/2;
     }
-
-    public void setFrq(double frq) {
-        this.frq = frq;
+    
+    public String mostraResultadoG1(){
+        if (calculaG1()>=7 && frequencia>=75) return "Aprovado em G1";
+        else if (calculaG1()>=4 && frequencia>=75) return "Em g2";
+        else
+            return "Reprovado";
     }
-
-    public double calculaG1() {
-        return (p1 + p2 + p3 + me) / 4;
+    
+    public String mostraResultadoG2(){
+        if (calculaG2()>=5  && frequencia>=75) return "Aprovado em G2";
+        else
+            return "Reprovado";
     }
-
-    public double calculaG2() {
-        return (calculaG1() + g2) / 2;
-    }
-
-    public String showG1() {
-        if (calculaG1() >= 7 && frq >= 75)
-            return "Aluno aprovado";
-        else if (calculaG1() < 7 && frq >= 75)
-            return "Aluno em G2";
-        else 
-            return "Aluno reprovado";
-    }
-
-    public String showG2() {
-        if (calculaG2() >= 5)
-            return "Aluno aprovado";
-        else 
-            return "Aluno reprovado";
+    
+    public String toString(){
+        return "Resultado do G1:"+ calculaG1() + "\nResultado do G2:" + mostraResultadoG2();
     }
 }
-...
