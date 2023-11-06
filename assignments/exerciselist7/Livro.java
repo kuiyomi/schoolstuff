dea634b9310d281a40bf4adbd1af13eb86f6a601
kuiyomi <141984045+kuiyomi@users.noreply.github.com>
2023-11-06 18:52:37 -0300

    Update Livro.java

--- assignments/exerciselist7/Livro.java
+++ assignments/exerciselist7/Livro.java
@@ -1,1 +1,50 @@
+public class Livro {
+    private String titulo;
+    private String autores;
+    private int anoPublic;
+    private int codigo;
 
+    public Livro (String titulo, String autores, int anoPublic, int codigo) {
+        this.titulo = titulo;
+        this.autores = autores;
+        this.anoPublic = anoPublic;
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    public int getAnoPublic() {
+        return anoPublic;
+    }
+
+    public void setAnoPublic(int anoPublic) {
+        this.anoPublic = anoPublic;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String toString() {
+        return ("Codigo: " + codigo + "\nTitulo: " + titulo + "\nAno de publicação: " + anoPublic + "\nAutores: " + autores);
+    }
+
+}
