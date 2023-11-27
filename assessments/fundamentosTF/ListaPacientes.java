52a3582886d8b0579e9cd34a5452ebcc1438e152
kuiyomi <141984045+kuiyomi@users.noreply.github.com>
2023-11-27 17:08:18 -0300

    Create ListaPacientes.java

---
+++ assessments/fundamentosTF/ListaPacientes.java
@@ -0,0 +1,117 @@
+public class ListaPacientes {
+    private Paciente[] pacientes;
+    private int proximoIndice;
+
+    public ListaPacientes(int tamanho) {
+        pacientes = new Paciente[tamanho];
+        proximoIndice = 0;
+    }
+
+    public int getProximoIndice() {
+        return proximoIndice;
+    }
+
+    public Paciente consultaPacientePorNome(String nome) {
+        for (int i = 0; i < proximoIndice; i++) {
+            if (pacientes[i].getNome().equalsIgnoreCase(nome)) {
+                return pacientes[i];
+            }
+        }
+        return null;
+    }
+
+    public Paciente consultaPacientePorCodigo(int codigo) {
+        for (int i = 0; i < proximoIndice; i++) {
+            if (pacientes[i].getCodigo() == codigo) {
+                return pacientes[i];
+            }
+        }
+        return null;
+    }
+
+    public Paciente[] consultaPacientesIdadeAcima(int idade) {
+        Paciente[] sec = new Paciente[proximoIndice];
+        int proximoIndiceSEC = 0;
+        for (int i = 0; i < proximoIndice; i++) {
+            if (pacientes[i].getIdade() >= idade) {
+                sec[proximoIndiceSEC] = pacientes[i];
+                proximoIndiceSEC++;
+            }
+        }
+        return sec;
+    }
+
+    public boolean cadastraPaciente(Paciente paciente) {
+        if (proximoIndice == 0) {
+            pacientes[proximoIndice] = paciente;
+            proximoIndice++;
+            return true;
+        } else {
+            for (int i = 0; i < proximoIndice; i++) {
+                if (pacientes[i].getCodigo() == paciente.getCodigo() || pacientes[i].getNome().equalsIgnoreCase(paciente.getNome()));
+                    return false;
+            }
+            pacientes[proximoIndice] = paciente;
+            proximoIndice++;
+            return true;
+        }
+    }
+
+    public Paciente[] listaSecundaria() {
+        Paciente[] terc = new Paciente[proximoIndice];
+        for (int i = 0; i < proximoIndice; i++) {
+            terc[i] = pacientes[i];
+        }
+        return terc;
+    }
+
+    public boolean alteraPeso(int codigo, double peso) {
+        if (proximoIndice == 0) {
+            return false;
+        } else {
+            for (int i = 0; i < proximoIndice; i++) {
+                if (pacientes[i].getCodigo() == codigo) {
+                    pacientes[i].setPeso(peso);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public boolean alteraAltura(int codigo, double altura) {
+        if (proximoIndice == 0) {
+            return false;
+        } else {
+            for (int i = 0; i < proximoIndice; i++) {
+                if (pacientes[i].getCodigo() == codigo) {
+                    pacientes[i].setAltura(altura);
+                }
+            }
+            return false;
+        }
+    }
+
+    public boolean alteraSexo(int codigo, String genero) {
+        if (proximoIndice == 0) {
+            return false;
+        } else {
+            for (int i = 0; i < proximoIndice; i++) {
+                if (pacientes[i].getCodigo() == codigo) {
+                    pacientes[i].setGenero(genero);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public double mostraIMC(int codigo) {
+        for (int i = 0; i <proximoIndice; i++) {
+            if (pacientes[i].getCodigo() == codigo) {
+                return pacientes[i].calculaIMC();
+            }
+        }
+        return 0;
+    }
+}
